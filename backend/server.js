c360f89d4dc67ebfe7340b138024d1974d5f32f8
Your Name <your.email@example.com>
2025-11-21 23:22:11 +0545

    first commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import http from 'node:http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin:'*',
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log('user connected',socket.id)
+
+    // emit for the send requset .. this is emiting data and this io for public
+    // io.emit('public:emit',`${socket.id} this is connect to this group`)
+
+    // socket.emit('private:emit',`this is private`)
+    socket.on('message',(data)=>{
+        socket.to(data.id).emit('personal:message',data.msg)
+    })
+
+    // this is for group
+
+    socket.on('join:room',(data)=>{
+        socket.join(data)
+    })
+  
+    socket.on('disconnect',()=>{
+        console.log(`socket id ${socket.id}`)
+    })
+})
+
+
+server.listen(3000,()=>{
+    console.log('server is running')
+})
